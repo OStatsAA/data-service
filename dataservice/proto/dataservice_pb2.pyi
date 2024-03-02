d2059ed687baa9ee716edb3ecfa93da997227c4f
Guilherme <g.lisboa.oliveira@outlook.com>
2024-03-02 15:00:58 +0000

    fix: remove unused protobuf struct.proto

--- dataservice/proto/dataservice_pb2.pyi
+++ dataservice/proto/dataservice_pb2.pyi
@@ -1,4 +1,3 @@
-from google.protobuf import struct_pb2 as _struct_pb2
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
 from typing import ClassVar as _ClassVar, Optional as _Optional
