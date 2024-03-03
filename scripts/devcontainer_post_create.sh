0761575556240648a648ab80abac884608ef3754
Guilherme <g.lisboa.oliveira@outlook.com>
2024-03-03 21:22:17 +0000

    chore: update post create script and add local env vars to config

--- scripts/devcontainer_post_create.sh
+++ scripts/devcontainer_post_create.sh
@@ -5,3 +5,10 @@ poetry install
 
 # Activate .venv
 source .venv/bin/activate
+
+# Install localstack awslocal cli
+pip install awscli-local[ver1]
+
+# Create datasets bucket in localstack s3
+awslocal --endpoint-url=http://s3:4566 s3 mb s3://raw-datasets-uploads
+awslocal --endpoint-url=http://s3:4566 s3 mb s3://datasets
